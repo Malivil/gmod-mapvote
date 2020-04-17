63feba21027224bb57b244547465ddbc50397d42
Malivil <mlebson@outlook.com>
2020-04-16 22:37:02 -0400

    Updated batch file

--- CreateAddonGMA.bat
+++ CreateAddonGMA.bat
@@ -1,2 +1,6 @@
 "C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmad.exe" create -folder %1
+echo Removing old file
+del gmod-mapvote.gma
+echo Renaming file
+ren %1.gma gmod-mapvote.gma
 pause
