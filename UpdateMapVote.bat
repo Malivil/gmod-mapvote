ddbd26c42b301895c3b66be3df2af4ecee731f12
Malivil <mlebson@outlook.com>
2020-10-15 15:48:46 -0400

    Moved distribution scripts to their own repo

--- UpdateMapVote.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-set /p changes="Enter Changes: "
-"C:\Program Files (x86)\Steam\steamapps\common\GarrysMod\bin\gmpublish.exe"  update -addon "gmod-mapvote.gma" -id "1472570320" -changes "%changes%"
-pause
