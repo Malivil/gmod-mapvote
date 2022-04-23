db4aa0060b45d56b59bed39c5bbe4c3297be8627
Malivil <mlebson@outlook.com>
2022-04-23 09:14:26 -0400

    Tab -> Space

--- lua/ulx/modules/sh/mapvote.lua
+++ lua/ulx/modules/sh/mapvote.lua
@@ -1,13 +1,13 @@
 local CATEGORY_NAME = "MapVote"
 ------------------------------ VoteMap ------------------------------
 function AMB_mapvote( calling_ply, votetime, should_cancel )
-	if not should_cancel then
-		MapVote.Start(votetime, nil, nil, nil)
-		ulx.fancyLogAdmin( calling_ply, "#A called a votemap!" )
-	else
-		MapVote.Cancel()
-		ulx.fancyLogAdmin( calling_ply, "#A canceled the votemap" )
-	end
+    if not should_cancel then
+        MapVote.Start(votetime, nil, nil, nil)
+        ulx.fancyLogAdmin( calling_ply, "#A called a votemap!" )
+    else
+        MapVote.Cancel()
+        ulx.fancyLogAdmin( calling_ply, "#A canceled the votemap" )
+    end
 end
 
 local mapvotecmd = ulx.command( CATEGORY_NAME, "mapvote", AMB_mapvote, "!mapvote" )
